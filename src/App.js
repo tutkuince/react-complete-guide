f226fecd821674c940ecbfaefadbb3b9f59fdc6f
tutkuince <tutku.ince@outlook.com>
2019-03-15 21:21:10 +0300

    Creating a Functional Component with Person.js and usage of that component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import Person from './Person/Person';   // .js added automatically to Person
 
 class App extends Component {
-  render() {
-    // This one is JSX Code
-    return (
-        // JSX expression mush have one root element
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-      </div>
-    );
-    // JSX Code compile to this code.
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'));
-  }
+    render() {
+        // This one is JSX Code
+        return (
+            // JSX expression mush have one root element
+            <div className="App">
+                <h1>Hi, I'm a React App</h1>
+                <Person/>
+            </div>
+        );
+        // JSX Code compile to this code.
+        // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'));
+    }
 }
 
 export default App;
