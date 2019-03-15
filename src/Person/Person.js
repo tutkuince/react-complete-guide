259cfbf110e5d4cdd9436a7b70ae6eb45ab76400
tutkuince <tutku.ince@outlook.com>
2019-03-15 21:28:09 +0300

    Creating Components info

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+// When creating components, you have the choice between two different ways:
+// 1. Functional Components     => const com => { return <div>some JSX</div> }
+// 2. Class-based Components    => class Cmp extends Component { render () { return <div>some JSX</div> } }
+
 const person = () => {
     return <p>I'm a Person!</p>
 };
