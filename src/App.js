4b28460c8151ef0308bdf7622e7f4c5263bc818d
tutkuince <tutku.ince@outlook.com>
2019-03-15 21:09:37 +0300

    JSX Restrictions info

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ class App extends Component {
   render() {
     // This one is JSX Code
     return (
+        // JSX expression mush have one root element
       <div className="App">
         <h1>Hi, I'm a React App</h1>
       </div>
