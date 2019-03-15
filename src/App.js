c48adeae237c17802a32b29d0cb534fda78c0071
tutkuince <tutku.ince@outlook.com>
2019-03-15 21:42:26 +0300

    Working With Props

--- src/App.js
+++ src/App.js
@@ -9,7 +9,10 @@ class App extends Component {
             // JSX expression mush have one root element
             <div className="App">
                 <h1>Hi, I'm a React App</h1>
-                <Person/>
+                <Person name="Tutku" age="30" />
+                <Person name="Emin" age="29" />
+                <Person name="Musa" age="33" />
+                <Person name="Utku" age="21">My hobbies: Racing</Person>
             </div>
         );
         // JSX Code compile to this code.
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,10 +4,12 @@ import React from 'react';
 // 1. Functional Components     => const com => { return <div>some JSX</div> }
 // 2. Class-based Components    => class Cmp extends Component { render () { return <div>some JSX</div> } }
 
-const person = () => {
-    return <p>I'm a Person! and I am {Math.floor(Math.random() * 30)} years old!</p>
+const person = (props) => {
+    // return <p>I'm a Person! and I am {Math.floor(Math.random() * 30)} years old!</p>
     // without curly braces 'Math.floor(Math.random() * 30)' compiles like a text
     // We need curly braces for dynamic content
+
+    return <p>I'm {props.name}! and I am {props.age} years old!</p>
 };
 
 export default person;
